2055592586eb7a2f1bcf3d9bf0cb2132a69ee299
James C-D <51571627+jamescd18@users.noreply.github.com>
2024-01-26 21:40:38 -0500

    Add custom js code

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,28 @@ myImage.onclick = () => {
     myImage.setAttribute('src', 'images/puppies1.jpg')
   }
 }
+
+let myButton = document.querySelector('button')
+let myHeading = document.querySelector('h1')
+
+function setUserName() {
+  const myName = prompt('Please enter your name.')
+  if (!myName) {
+    localStorage.removeItem('name')
+    myHeading.textContent = `Dogs are cool!`
+  } else {
+    localStorage.setItem('name', myName)
+    myHeading.textContent = `Dogs are cool, ${myName}`
+  }
+}
+
+if (!localStorage.getItem('name')) {
+  myHeading.textContent = `Dogs are cool!`
+} else {
+  const storedName = localStorage.getItem('name')
+  myHeading.textContent = `Dogs are cool, ${storedName}`
+}
+
+myButton.onclick = () => {
+  setUserName()
+}
